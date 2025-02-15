e3aef3e5ff56dcfaf219814322ba5d8ea28ee508
Amishi Mishra <mishmish@unc.edu>
2025-02-15 14:25:36 -0500

    Create pearlHacked.py

---
+++ pearlHacked.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+from analysisFunctions import candleplot
+
+st.title('Stock Analyzer')
