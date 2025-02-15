b24a61d6838d9e89e9e6e54793205c6225b4ea46
Amishi Mishra <mishmish@unc.edu>
2025-02-15 12:59:29 -0500

    some stuff

--- amishiTries.py
+++ amishiTries.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 aapl = yf.Ticker('AAPL')
 
 st.write('I made something cool :p')
-st.write(aapl.hist)
+st.write(aapl.history(period = 'max'))
 
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
