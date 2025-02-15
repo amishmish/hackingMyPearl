6c1a446491cd4b8ba6050c99ba59b759c6101d9c
Amishi Mishra <mishmish@unc.edu>
2025-02-15 12:53:00 -0500

    Update amishiTries.py

---
+++ amishiTries.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
+aapl = yf.Ticker('AAPL')
+
+st.write('I made something cool :p')
+st.write(aapl.hist)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
