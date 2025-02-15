50d7acbf8dd2fc51cf96be96718ae18c59d16d98
Amishi Mishra <mishmish@unc.edu>
2025-02-15 15:25:11 -0500

    Update pearlHacked.py

--- pearlHacked.py
+++ pearlHacked.py
@@ -6,3 +6,15 @@ import matplotlib.pyplot as plt
 from analysisFunctions import candleplot
 
 st.title('Stock Analyzer')
+
+st.text_input("Please enter a stock ticker from the Yahoo Finance library", key="ticker")
+
+stock = yf.Ticker(st.session_state.ticker)
+hist = stock.history(period='6mo')
+plot1 = candleplot(hist, 'green', 'red')
+
+if not hist.empty:
+    st.write(hist)
+    st.pyplot(plot1)
+else:
+    st.write(st.session_state.ticker + " is not in the Yahoo Finance Library")
